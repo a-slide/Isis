cdb255c9da5fba86edc49818e606cb0b0d9fd423
Adrien Leger <adrien.leger@inserm.fr>
2014-04-17 09:43:11 +0200

    Cleanup

--- TestFun.py
+++
@@ -1,7 +0,0 @@
-from ReferenceJunctions import ReferenceJunctions
-from ReferenceGenome import ReferenceGenome
-from SlicePicker import SlicePicker
-
-r1 = ReferenceGenome("Mouse","datasets/small_multi.fa")
-r2 = ReferenceGenome("AAV","datasets/Bacterial_backbone.fa")
-j = ReferenceJunctions ("True_junctions", 20 , 200, 200, r1, r2, True, False)
